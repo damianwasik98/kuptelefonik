0f3bc65a1919513c363e4c4f7ad6a3f1421e0a4e
Damian Wąsik <damianwasik98@gmail.com>
2021-04-06 20:35:22 +0200

    Added 2 more views and implemented 2 inherited templates

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('search', views.search, name='search'),
+    path('observed', views.observed, name='observed')
 ]
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def search(request):
+    return render(request, 'search.html')
+
+def observed(request):
+    return render(request, 'observed.html')
