3f35f1ed42544b498667697de7c5ecf0ed26fdb5
Damian Wąsik <damianwasik98@gmail.com>
2021-04-24 17:24:33 +0200

    Implemented rest api chart data view with some dummy data

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,14 @@ def search(request):
 
 def observed(request):
     return render(request, 'observed.html')
+
+class PhonePriceChartData(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        chart_data = {
+            'prices': ['3449.49', '3134.45'] 
+        }
+        return JsonResponse(chart_data)
