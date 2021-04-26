01c575b708488d39d88f0f2cf712840fd5011bdc
Damian Wąsik <damianwasik98@gmail.com>
2021-04-26 10:11:37 +0200

    Implemented first data from model in template

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 
+from .models import Phone
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def search(request):
     query = request.GET.get('query')
-    return render(request, 'search.html', {'query': query})
+    search_result = Phone.objects.all()
+    return render(request, 'search.html', {'query': query, 'results': search_result})
 
 def observed(request):
     return render(request, 'observed.html')
