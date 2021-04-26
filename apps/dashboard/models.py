612c4e33663c6bd632d654e0441c8d36e29af1a1
Damian Wąsik <damianwasik98@gmail.com>
2021-04-26 11:14:33 +0200

    Added image field in Phone model

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -7,6 +7,7 @@ class Phone(models.Model):
     storage = models.IntegerField()
     storage_unit = models.CharField(max_length=50)
     color = models.CharField(max_length=100, blank=True)
+    img = models.ImageField(upload_to=f'phones', blank=True)
 
     def __str__(self):
         return f'{self.name} {self.storage}{self.storage_unit}'
