df1493166435755916af48a002c8cd05695f1d2d
Damian Wąsik <damianwasik98@gmail.com>
2021-09-18 01:18:32 +0200

    Fixed bug with temporary unavailable phone on rtv euro agd (#81)

--- src/data_scraping/data_scraping/scrapers/rtv_euro_agd.py
+++ src/data_scraping/data_scraping/scrapers/rtv_euro_agd.py
@@ -6,7 +6,7 @@ from typing import Optional
 import demjson
 
 from data_scraping.scrapers.base_scraper import PhoneOfferScraper
-from data_scraping.scrapers.exceptions import PriceNotFoundOnPage
+from data_scraping.scrapers.exceptions import PhoneUnavailable, PriceNotFoundOnPage
 
 logger = logging.getLogger(__name__)
 
@@ -24,15 +24,21 @@ class RTVEuroAGDScraper(PhoneOfferScraper):
     def extract_price_from_product_dict(product_dict: dict) -> Decimal:
         return Decimal(product_dict["price"])
 
+    @staticmethod
+    def is_phone_available(product_dict: dict) -> bool:
+        return not product_dict["unavailableAtTheMoment"]
+
     def extract_product_dict_from_html(self) -> Optional[dict]:
-        product_js_dict_regex = re.compile(r"_PRODUCT=({[^;]+});")
+        product_js_dict_regex = re.compile(r"(?<=app\.pageConfig\(\"productCard\",\ ){[^;]+}(?=\);)")
         match = re.search(pattern=product_js_dict_regex, string=self.offer_page_html)
         if not match:
             raise PriceNotFoundOnPage(page_html=self.offer_page_html)
-        product_js_dict = match.group(1)
+        product_js_dict = match.group()
         product_parsed_dict = self._parse_js_dict(product_js_dict)
         return product_parsed_dict
 
     def get_price(self) -> Decimal:
         product_dict = self.extract_product_dict_from_html()
+        if not self.is_phone_available(product_dict):
+            raise PhoneUnavailable(page_html=self.offer_page_html)
         return self.extract_price_from_product_dict(product_dict)
