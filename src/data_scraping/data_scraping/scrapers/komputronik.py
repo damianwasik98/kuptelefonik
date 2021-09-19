c15be6e8d43b28c77be6cbb5a980d055dea4d9dd
Damian Wąsik <damianwasik98@gmail.com>
2021-09-20 00:48:51 +0200

    Fixed bug with unavailable phone on komputronik (#90)

--- src/data_scraping/data_scraping/scrapers/komputronik.py
+++ src/data_scraping/data_scraping/scrapers/komputronik.py
@@ -1,12 +1,18 @@
 from decimal import Decimal
+from enum import Enum
 import json
 import logging
 import re
 
 from data_scraping.scrapers.base_scraper import PhoneOfferScraper
-from data_scraping.scrapers.exceptions import PriceNotFoundOnPage
+from data_scraping.scrapers.exceptions import PhoneUnavailable, PriceNotFoundOnPage
 
 logger = logging.getLogger(__name__)
+
+
+class AvailabilityStatus(Enum):
+    AVAILABLE = "available"
+    UNAVAILABLE = "unavailable"
 
 
 class KomputronikScraper(PhoneOfferScraper):
@@ -23,6 +29,19 @@ class KomputronikScraper(PhoneOfferScraper):
         extracted_price = event_dict["ecommerce"]["detail"]["products"][0]["price"]
         return Decimal(extracted_price)
 
+    @staticmethod
+    def is_phone_available(event_dict: dict) -> bool:
+        status = event_dict["ecommerce"]["detail"]["products"][0]["availability"]
+        enum_values = AvailabilityStatus._value2member_map_.keys()
+
+        if status not in enum_values:
+            raise NotImplementedError(f"Unknown status {status}")
+        
+        if status == AvailabilityStatus.AVAILABLE.value:
+            return True
+        return False
+
+
     def extract_ecommerce_js_event_dict(self):
         js_event_json_regex = re.compile(r"dataLayer\.push\(({[^;]+})\);")
         match = re.search(pattern=js_event_json_regex, string=self.offer_page_html)
@@ -34,4 +53,6 @@ class KomputronikScraper(PhoneOfferScraper):
 
     def get_price(self) -> Decimal:
         event_dict = self.extract_ecommerce_js_event_dict()
+        if not self.is_phone_available(event_dict):
+            raise PhoneUnavailable(page_html=self.offer_page_html)
         return self.extract_price_from_event_dict(event_dict)
